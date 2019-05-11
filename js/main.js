6d876f9fd58fae8768a5eff9cfaf840caf346c4d
vaishak.k <vaishak.k@zohocorp.com>
2019-05-12 00:33:33 +0530

    added copy option and submit on enter. Also URL wont affect the original URL in the address bar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var endPoint = "https://www.jsonstore.io/114384795ac84252a99ae5a943c3645c36534f10bb4f3ce909f1699d9cf34a79";
 
+// For Executing the function on ENTER.
+var input = document.getElementById("urlinput");
+
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("shorten").click();
+    }
+});
+
 //function for getting random url string.
 function getrandom() {
     var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
@@ -30,20 +41,23 @@ function geturl() {
 //for creating the hash that is the anchor part of any URL.
 function genhash() {
     if (window.location.hash == "") {
-        window.location.hash = getrandom();
+        this.shortHash = getrandom();
     }
 }
 
-//for sending the hash and the long url to the json end point.
+//for sending the hash and the long url to the json end point. Also clearing the input.
 function send_request(url) {
     this.url = url;
     $.ajax({
-        'url': endPoint + "/" + window.location.hash.substr(1),
+        'url': endPoint + "/" + this.shortHash,
         'type': 'POST',
         'data': JSON.stringify(this.url),
         'dataType': 'json',
         'contentType': 'application/json; charset=utf-8'
     })
+
+    var input = document.getElementById("urlinput");
+    input.value = "";
 }
 
 //Main function.
@@ -51,8 +65,11 @@ function shorturl() {
     var longurl = geturl();
     genhash();
     send_request(longurl);
+    simplecopy(window.location.href + '#' + this.shortHash);
+    alert();
 }
 
+//for redirecting any link that has been entered with any hash to its original lengthy URL.
 var hashh = window.location.hash.substr(1)
 
 if (window.location.hash != "") {
@@ -65,3 +82,50 @@ if (window.location.hash != "") {
 
     });
 }
+
+//for displaying the url has been shortened alert at the bottom.
+function alert() {
+    var alertPlaceHolder = document.getElementById('alertdialog');
+    alertPlaceHolder.innerHTML = "URL Shortened and Copied.";
+    setTimeout(function() {
+        alertPlaceHolder.innerHTML = "";
+    }, 5000);
+}
+
+
+
+//Simple Copy which is used for copying the shortened URL to your clipboard which can be simply used by pasting it.
+var simplecopy = function() { // make a closure so we don't pollute the namespace
+    var node = document.createElement("DIV");
+    node.style.position = "fixed"; // doesn't interact with the DOM
+    node.style.color = "rgba(0,0,0,0)"; // transparent
+
+    function simplecopy(copyText, callback) {
+        node.innerText = copyText;
+        document.body.appendChild(node);
+        var selectStatus = selectText(node);
+        var copyStatus = document.execCommand("copy", false, null); // copy currently selected text
+        document.body.removeChild(node);
+        return selectStatus && copyStatus;
+    }
+
+    function selectText(node) {
+        var range, selection;
+        if (document.body.createTextRange) { // if the browser supports this old method
+            range = document.body.createTextRange(); // create a text range
+            range.moveToElementText(node);
+            range.select();
+            return true;
+        } else if (window.getSelection) { // if the browser supports the newer method
+            selection = window.getSelection(); // get a selection object
+            range = document.createRange(); // create a blank range object
+            range.selectNodeContents(node);
+            selection.removeAllRanges(); // clear any current range from the selection
+            selection.addRange(range); // select the contents of the range
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return simplecopy;
+}();
